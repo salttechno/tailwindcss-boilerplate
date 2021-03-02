392bef30c1c1ed9fc3bbf1d0f04d3d7ecf6561d3
Nilesh G <nileshg@salttechno.com>
2021-03-02 11:22:04 +0530

    Add comments

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,16 @@ var browserSync = require("browser-sync").create();
 var postcss = require("gulp-postcss");
 const imagemin = require("gulp-imagemin");
 
+// Task for compiling our CSS files using PostCSS
 function cssTask(cb) {
-	return src("./src/*.css")
-		.pipe(postcss())
-		.pipe(dest("./assets/css"))
+	return src("./src/*.css") // read .css files from ./src/ folder
+		.pipe(postcss()) // compile using postcss
+		.pipe(dest("./assets/css")) // paste them in ./assets/css folder
 		.pipe(browserSync.stream());
 	cb();
 }
 
+// Task for minifying images
 function imageminTask(cb) {
 	return src("./assets/images/*")
 		.pipe(imagemin())
@@ -32,7 +34,7 @@ function browsersyncReload(cb) {
 	cb();
 }
 
-// Watch Task
+// Watch Files & Reload browser after tasks
 function watchTask() {
 	watch("./**/*.html", browsersyncReload);
 	watch(["./src/*.css"], series(cssTask, browsersyncReload));
